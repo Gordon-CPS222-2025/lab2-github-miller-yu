a5a7bc11c50ae346524a08b3564029259069f2ac
Josh Miller <josh.miller@hosea.gordon.edu>
2025-01-28 11:16:26 -0500

    Initial commit

--- tictactoe_test.cc
+++ tictactoe_test.cc
@@ -26,24 +26,6 @@ bool tttb_winner_vertical_test() {
 }
 
 
-bool tttb_winner_diagonal_test() {
-    TictactoeBoard b;
-    b.set(0, 0, TictactoeXO::X);
-    b.set(1, 1, TictactoeXO::X);
-    if (TictactoeXO::X == b.winner()) {
-        cout << "tttb_winner_diagonal_test() failed for 2 in a row" << endl;
-        return false;
-    }
-    b.set(2, 2, TictactoeXO::X);
-    if (TictactoeXO::X != b.winner()) {
-        cout << "tttb_winner_diagonal_test() failed for 3 in a row" << endl;
-        return false;
-    }
-    return true;
-}
-
-
-
 // Runs tests above, and prints a message if they all pass.
 // If any fail, they print their own failure messages.
 int main() {
